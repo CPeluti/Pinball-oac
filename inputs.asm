5c29de5d20eee5a1bcb712ebc84e799d3eec3425
Caio <cpeluti@gmail.com>
2021-05-17 20:12:25 +0000

    inputs retorna resultado em a0

--- inputs.asm
+++ inputs.asm
@@ -10,6 +10,6 @@ KEY2:	li t1,0xFF200000		# carrega o endereço de controle do KDMMIO
 	lw t0,0(t1)			# Le bit de Controle Teclado
 	andi t0,t0,0x0001		# mascara o bit menos significativo
    	beq t0,zero,FIM   	   	# Se não há tecla pressionada então vai para FIM
-  		lw t2,4(t1)  			# le o valor da tecla tecla
+  		lw a0,4(t1)  			# le o valor da tecla tecla
 	
 FIM:	ret	
